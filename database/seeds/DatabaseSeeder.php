e19affd63a2b60192b62b6412e26ad889dda3b3c
Chanatip Tippayakul <chanatipt@gmail.com>
2020-07-21 21:42:00 +0700

    change default db

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         $this->call(UserSeeder::class);
+//         $this->call(UserSeeder::class);
 		/* fbsg-signature-addSeedTables:<begin> */
          $this->call(taskStatusTableSeeder::class);
          $this->call(RoleTableSeeder::class);
